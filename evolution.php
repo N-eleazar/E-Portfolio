53257b0324d211ac367709c5683bd659a67f6626
Eleazar Nabet <nabet.eleazar@gmail.com>
2022-02-26 11:38:33 +0100

    separation des pages

--- evolution.php
+++ evolution.php
@@ -73,55 +73,11 @@
 
       <section>
         <article>
-          <div class="row">
-            <div class="col">
-              <h2 id="services">MES SERVICES</h2>
-              <div class="service">
-                <aside class="row">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-                  porttitor hendrerit nibh non ultricies. Phasellus eu condimentum
-                  odio. Proin in magna vel dui semper elementum. Donec pulvinar nibh
-                  vel nisl varius vehicula. Sed efficitur, augue vel consectetur
-                  sollicitudin, ipsum dui interdum justo, vitae porta arcu diam ut
-                  leo. Morbi lorem massa, sollicitudin quis nunc id, vestibulum
-                  tincidunt mauris. Curabitur quis nisl sit amet nisi bibendum porta
-                  sit amet scelerisque elit.
-                  <br />
-                  Maecenas elementum mauris vitae arcu mollis, sed interdum erat
-                  porttitor. Aliquam sit amet urna interdum arcu interdum dictum vitae
-                  sit amet purus. Mauris aliquam mauris ante, nec faucibus lectus
-                  tempus vitae. Nunc nisi est, scelerisque non accumsan vel, tristique
-                  quis turpis. In non sem rutrum, luctus nisi vel, efficitur arcu.
-                  Pellentesque et sem risus. Nulla erat orci, luctus et scelerisque
-                  id, elementum vel lectus. Donec eleifend molestie egestas. Lorem
-                  ipsum dolor sit amet, consectetur adipiscing elit.
-                  <br />
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-                  porttitor hendrerit nibh non ultricies. Phasellus eu condimentum
-                  odio. Proin in magna vel dui semper elementum. Donec pulvinar nibh
-                  vel nisl varius vehicula. Sed efficitur, augue vel consectetur
-                  sollicitudin, ipsum dui interdum justo, vitae porta arcu diam ut
-                  leo. Morbi lorem massa, sollicitudin quis nunc id, vestibulum
-                  tincidunt mauris. Curabitur quis nisl sit amet nisi bibendum porta
-                  sit amet scelerisque elit.
-                  <br />
-                  Maecenas elementum mauris vitae arcu mollis, sed interdum erat
-                  porttitor. Aliquam sit amet urna interdum arcu interdum dictum vitae
-                  sit amet purus. Mauris aliquam mauris ante, nec faucibus lectus
-                  tempus vitae. Nunc nisi est, scelerisque non accumsan vel, tristique
-                  quis turpis. In non sem rutrum, luctus nisi vel, efficitur arcu.
-                  Pellentesque et sem risus. Nulla erat orci, luctus et scelerisque
-                  id, elementum vel lectus. Donec eleifend molestie egestas. Lorem
-                  ipsum dolor sit amet, consectetur adipiscing elit.
-                  <br />
-                </p>
-              </aside>
-            </div>
-        </div>
-      </article>
+            <?php 
+                require 'valeurs.php'; 
+                require 'mon_evolution.php'; 
+            ?>
+        </article>
       </section>
 
       <footer>
@@ -129,15 +85,12 @@
           <div class="col">
             <div class="evolution">
               <div class="row">
-
               </div>
             </div>
           </div>
         </div>
       </footer>
-
     </div>   <!-- Fin Container -->
-
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
