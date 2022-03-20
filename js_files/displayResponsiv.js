80b3e005b55a18752f38734470c567f91ca67ed5
Eleazar Nabet <nabet.eleazar@gmail.com>
2022-03-20 20:32:44 +0100

    80% site realisé

--- js_files/displayResponsiv.js
+++ js_files/displayResponsiv.js
@@ -9,34 +9,34 @@ function displayProject(btnId) {
 switch(btnId) {
   case "site1":
     document.getElementById("desktop").src = url1
-    document.getElementById('btn1').className = "btn btn-info"
-    document.getElementById('btn2').className = "btn btn-primary"
-    document.getElementById('btn3').className = "btn btn-primary"
-    document.getElementById('btn4').className = "btn btn-primary"
+    document.getElementById('btn1').className = "btn-info"
+    document.getElementById('btn2').className = "btn-primary"
+    document.getElementById('btn3').className = "btn-primary"
+    document.getElementById('btn4').className = "btn-primary"
     break;
 
   case "site2":
     document.getElementById("desktop").src = url2
-    document.getElementById('btn1').className = "btn btn-primary"
-    document.getElementById('btn2').className = "btn btn-info" 
-    document.getElementById('btn3').className = "btn btn-primary"
-    document.getElementById('btn4').className = "btn btn-primary"
+    document.getElementById('btn1').className = "btn-primary"
+    document.getElementById('btn2').className = "btn-info" 
+    document.getElementById('btn3').className = "btn-primary"
+    document.getElementById('btn4').className = "btn-primary"
     break;
 
   case "site3":
     document.getElementById("desktop").src = url3
-    document.getElementById('btn1').className = "btn btn-primary"
-    document.getElementById('btn2').className = "btn btn-primary"
-    document.getElementById('btn3').className = "btn btn-info"
-    document.getElementById('btn4').className = "btn btn-primary"
+    document.getElementById('btn1').className = "btn-primary"
+    document.getElementById('btn2').className = "btn-primary"
+    document.getElementById('btn3').className = "btn-info"
+    document.getElementById('btn4').className = "btn-primary"
     break; 
 
   case "site4":
     document.getElementById("desktop").src = url4
-    document.getElementById('btn1').className = "btn btn-primary"
-    document.getElementById('btn2').className = "btn btn-primary"
-    document.getElementById('btn3').className = "btn btn-primary"
-    document.getElementById('btn4').className = "btn btn-info"
+    document.getElementById('btn1').className = "btn-primary"
+    document.getElementById('btn2').className = "btn-primary"
+    document.getElementById('btn3').className = "btn-primary"
+    document.getElementById('btn4').className = "btn-info"
     break;
 
 }
