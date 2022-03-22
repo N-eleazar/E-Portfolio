0709d037dd1ba9a22d98d4c2ee837ca5c8ca94bf
Eleazar Nabet <nabet.eleazar@gmail.com>
2022-03-22 05:13:56 +0100

    modiff page Evloution & Roadmap

--- evolution.php
+++ evolution.php
@@ -1,62 +1,91 @@
 <!DOCTYPE html>
-<html lang="fr">
-  <head>
+<head>
+ <title>PortFolio</title>
+ 
+ <!--  Meta Link  -->
+ <meta charset="UTF-8">
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+ <link rel="shortcut icon" href="http://portfolio.fr.nf/sites/images/icon.png" type="image/vnd.microsoft.icon" />
+ <meta name="description" content="Tout sur ..." />
+ <meta name="robots" content="follow, index" />
+ <meta name="keywords" content="..." />
+ <meta name="news_keywords" content="..." />
+ <meta name="generator" content="Drupal 7 (http://drupal.org)" />
+ <link rel="canonical" href="http://portfolio.fr.nf/" />
+ <meta name="rights" content="2021 - portfolio" />
+ <link rel="shortlink" href="http://portfolio.fr.nf/" />
+ <meta name="original-source" content="http://portfolio.fr.nf" />
+ <meta property="fb:admins" content="eleazar.nabet" />
+ <meta name="revisit-after" content="1 day" />
+ <meta property="og:type" content="website" />
+ <meta property="og:site_name" content="portfolio" />
+ <meta property="og:url" content="http://portfolio.fr.nf/" />
+ <meta property="og:title" content="portfolio" />
+ <meta property="og:description" content="Tout sur ..." />
+ <meta property="og:image" content="http://portfolio.fr.nf/sites/images/icon.png" />
+ <meta name="twitter:card" content="summary" />
+ <meta name="twitter:site" content="@portfolio" />
+ <meta property="og:email" content="" />
+ <meta name="twitter:image" content="http://portfolio.fr.nf/sites/images/icon.png" />
+ <meta name="twitter:description" content="Tout sur ..." />
+ <meta property="og:phone_number" content="" />
+ <meta property="og:locale" content="fr_FR" />
+ <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+ <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+     
+ <!--  CSS Files  -->
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+ <link rel="stylesheet" href="css_files/style.css">
+ <link rel="stylesheet" href="css_files/w3.css">
+ <link rel="stylesheet" href="css_files/font-awesome.min.css">
+ 
+ <!--  JS Files  -->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+ <script src="js_files/bootstrap.bundle.min.js"></script>
+ <script src="js_files/script.js"></script>
+</head>
 
-    <!-- Meta Info  -->
-    <meta charset="UTF-8" />
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
-    <link rel="shortcut icon" href="http://portfolio.fr.nf/sites/images/icon.png" type="image/vnd.microsoft.icon" />
-    <meta name="description" content="Tout sur ..." />
-    <meta name="robots" content="follow, index" />
-    <meta name="keywords" content="..." />
-    <meta name="news_keywords" content="..." />
-    <meta name="generator" content="Drupal 7 (http://drupal.org)" />
-    <link rel="canonical" href="http://portfolio.fr.nf/" />
-    <meta name="rights" content="2021 - portfolio" />
-    <link rel="shortlink" href="http://portfolio.fr.nf/" />
-    <meta name="original-source" content="http://portfolio.fr.nf" />
-    <meta property="fb:admins" content="eleazar.nabet" />
-    <meta name="revisit-after" content="1 day" />
-    <meta property="og:type" content="website" />
-    <meta property="og:site_name" content="portfolio" />
-    <meta property="og:url" content="http://portfolio.fr.nf/" />
-    <meta property="og:title" content="portfolio" />
-    <meta property="og:description" content="Tout sur ..." />
-    <meta property="og:image" content="http://portfolio.fr.nf/sites/images/icon.png" />
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:site" content="@portfolio" />
-    <meta property="og:email" content="" />
-    <meta name="twitter:image" content="http://portfolio.fr.nf/sites/images/icon.png" />
-    <meta name="twitter:description" content="Tout sur ..." />
-    <meta property="og:phone_number" content="" />
-    <meta property="og:locale" content="fr_FR" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  
-    <!--   CSS Files   -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossorigin="anonymous"
-    />
-    <title>Mon PortFolio</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="/css_files/style.css" />
-    <link rel="stylesheet" href="/css_files/responsiv.css" />
-  
-    <!--   JS Files   -->
-    <script src="/js_files/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="/js_files/displayResponsiv.js"></script>
-  
-  </head>
-  <body>
-    <div class="container-fluid">
-      <header>
-      </header>
+<body>
+  <div class="container-fluid">
+    <!--   Header   -->
+    <header class="row">
+    <!--   Navbar   -->  
+      <div class="col col-10">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <a class="navbar-brand" href="#">Accueil</a>
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link" href="#about">A Propos</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#myself">Présentation</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#level">Competences</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#portfolio">Réalisations</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#contact">Contact</a>
+                </li>
+               
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div class="col col-2">
+        <button type="button" class="scroll" onclick="scrollToTop()">Scroll Top</button>
+      </div>
+    </header>
+
 
 <?php require 'valeurs.php'; ?>
 
@@ -191,20 +220,9 @@
   </div>
 </div>
   
-<footer>
-              <article id="footer">
-                <p class="copyright">Company Name © 2021</p>
-              </article>
-            </footer>
-            
-            </div>   <!-- Fin Container -->
-                
-            <!--   JS Files   -->
-            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-            <script src="/js_files/script.js"></script>
-            </body>
-            </html>
-            
-            
-            
+    <footer>
+      <p class="copyright">Company Name © 2021</p>
+    </footer>
+ </div>
+</body>
+</html>
