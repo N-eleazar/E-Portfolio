63972f5972cea067e27818aed59c2a28ab8f5c41
Eleazar Nabet <nabet.eleazar@gmail.com>
2022-03-27 22:16:02 +0200

    Refonte site

--- js_files/script.js
+++ js_files/script.js
@@ -2,6 +2,7 @@ const url1 = "http://gat-orane/"
 const url2 = "https://picsum.photos/"
 const url3 = "http://lowly.fr.nf/"
 const url4 = "https://jacekjeznach.com/"
+const url5 = "http://127.0.0.1:5500/home/"
 
 
 
@@ -15,6 +16,7 @@ switch(btnId) {
     document.getElementById('btn2').className = "btn-secondary"
     document.getElementById('btn3').className = "btn-secondary"
     document.getElementById('btn4').className = "btn-secondary"
+    document.getElementById('btn5').className = "btn-secondary"
     break;
 
   case "site2":
@@ -23,6 +25,7 @@ switch(btnId) {
     document.getElementById('btn2').className = "btn-info" 
     document.getElementById('btn3').className = "btn-secondary"
     document.getElementById('btn4').className = "btn-secondary"
+    document.getElementById('btn5').className = "btn-secondary"
     break;
 
   case "site3":
@@ -31,6 +34,7 @@ switch(btnId) {
     document.getElementById('btn2').className = "btn-secondary"
     document.getElementById('btn3').className = "btn-info"
     document.getElementById('btn4').className = "btn-secondary"
+    document.getElementById('btn5').className = "btn-secondary"
     break; 
 
   case "site4":
@@ -39,8 +43,16 @@ switch(btnId) {
     document.getElementById('btn2').className = "btn-secondary"
     document.getElementById('btn3').className = "btn-secondary"
     document.getElementById('btn4').className = "btn-info"
+    document.getElementById('btn5').className = "btn-secondary"
     break;
-
+  case "site5":
+    document.getElementById("desktop").src = url5
+    document.getElementById('btn1').className = "btn-secondary"
+    document.getElementById('btn2').className = "btn-secondary"
+    document.getElementById('btn3').className = "btn-secondary"
+    document.getElementById('btn4').className = "btn-secondary"
+    document.getElementById('btn5').className = "btn-info"
+    break;
 }
 }
 
