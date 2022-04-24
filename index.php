aabf236ffd5748a48ca88e9ae7d86c2cb2037463
NABET Eleazar <nabet.eleazar@gmail.com>
2022-04-24 23:08:42 +0200

    ajout tailwindcss

--- index.php
+++ index.php
@@ -10,112 +10,87 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!--  Tailwind CSS  -->
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!--  CSS Files  -->
     <link rel="stylesheet" href="css_files/style.css">
   </head>
   <body>
+    
   <div class="container-fluid">
-    <div class="row scroll">
-      <button id="button" class="btn">
-        <img src="images\scrollTop.png" 
-        alt="bouton Scroll Top"
-        width="40px">
-      </button>
+
+    <div class="scroll fixed bottom-5 right-5">
+      <img src="images\scrollTop.png" 
+      alt="bouton Scroll Top"
+      width="40px">
     </div>
+
   <!--  HEADER  -->
   <!--  #TODO Reparer bug du menu defilant responsive  -->
-  <header class="row bg-02">
+  <header class="row flex justify-center">
     <nav class="navbar navbar-expand-md navbar-light bg-02"> 
       <div class="container-fluid">
-        <div class="logo col-1"><img src="images/zozor_logo.png" alt=""></div>
+        <div class="flex justify-center align-center col-1"><img src="images/zozor_logo.png" alt=""></div>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <button class="btn btn-outline-secondary">
               <li class="nav-item">
                 <a class="nav-link" href="#about">Présentation</a>
               </li>
-            </button>
-            <button class="btn btn-outline-secondary">
               <li class="nav-item">
                 <a class="nav-link" href="#level">Mes Competences</a>
               </li>
-            </button>
-            <button class="btn btn-outline-secondary">
               <li class="nav-item">
                 <a class="nav-link" href="#portfolio">Mes Realisations</a>
               </li>
-            </button>
-            <button class="btn btn-outline-secondary">
               <li class="nav-item">
                 <a class="nav-link" href="#contact">Contact</a>
               </li>
-            </button>
           </ul>
         </div>
       </div>
     </nav>
   </header>
-  <!-- <hr> Bandeau -->
-  <div id="carouselExampleIndicators" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://cdn.infosec-ups.com/media/Solutions/02-reseaux-et-serveurs/01-environnement-virtuel/header-env-virtuel-2220x680px72dpi.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://cdn.infosec-ups.com/media/Solutions/02-reseaux-et-serveurs/01-environnement-virtuel/header-env-virtuel-2220x680px72dpi.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://cdn.infosec-ups.com/media/Solutions/02-reseaux-et-serveurs/01-environnement-virtuel/header-env-virtuel-2220x680px72dpi.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-  </div>
+
+  <!-- BANDEAU -->
+    <section class="row bg-02">
+      <div class="carousel-item active">
+        <img src="images/carousel.jpg" class="d-block w-100" alt="...">
+      </div>
+  </section>
 
   <!--  A PROPOS  -->
-  <section class="row about bg-01" id="about">   
-    <div class="col">
-      <h1>Hello,<br>
+  <section class="row bg-01" id="about">   
+      <h1 class="col text-red-800 text-5xl font-semibold text-center p-6">
+      Hello,<br>
       Je suis Eléazar<br>
       Développeur web / Front End.</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis exercitationem, sit iure, rem, beatae assumenda molestias nam soluta ad officia aliquam repellendus laboriosam sapiente consequuntur a dignissimos consectetur maxime atque.<br>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis exercitationem, sit iure, rem, beatae assumenda molestias nam soluta ad officia aliquam repellendus laboriosam sapiente consequuntur a dignissimos consectetur maxime atque.</p>
-    </div>
-    <div class="row myself">
-      <div class="col">
-        <p><img src="images/avatar_icon.png" alt="">Bienvenue sur mon site web. Je suis lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis exercitationem, sit iure, rem, beatae assumenda molestias nam soluta ad officia aliquam repellendus laboriosam sapiente consequuntur a dignissimos consectetur maxime atque.<br>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis exercitationem, sit iure, rem, beatae assumenda molestias nam soluta ad officia aliquam repellendus laboriosam sapiente consequuntur a dignissimos consectetur maxime atque.</p>
-      </div>
+
+    <div class="row">
+      <p class="col text-justify text-xl m-16">
+        <img class="float-left p-4" src="images/avatar_icon.png" alt="">
+        Bienvenue sur mon site web. Je suis lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis exercitationem, sit iure, rem, beatae assumenda molestias nam soluta ad officia aliquam repellendus laboriosam sapiente consequuntur a dignissimos consectetur maxime atque.<br>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis exercitationem, sit iure, rem, beatae assumenda molestias nam soluta ad officia aliquam repellendus laboriosam sapiente consequuntur a dignissimos consectetur maxime atque.</p>
     </div>
   </section>
+
+
   <!--  FRONT_END  -->
-  <section class="row level-front bg-02" id="level">
-    <h3>Mes Compétences Front</h3>
-    <div class="parcours-wrapper">
+  <section class="row flex justify-center bg-02" id="level">
+    <h3 class="text-red-800 pb-6">Mes Compétences Front</h3>
+    <div class="parcours-wrapper grid grid-cols-5">
       <?php
         include_once 'database/valeurs.php';
         $count = count($frontEnd);
         for ($i = 0; $i < $count; $i++) {
       ?>
-      <div class="panel panel-front col-lg-3 col-md-4 col-sm-6 col-xs-12">
-        <div class="panel-heading">
+      <div class="panel">
+        <div class="panel-heading h-3/5">
           <h4><?php echo $frontEnd[$i][0];?></h4>
         </div>
         <div class="panel-body">
@@ -132,21 +107,20 @@
           </div>
         </div>
       </div>
-      <?php
-          }
-      ?>
+      <?php } ?>
     </div>
   </section>
+
   <!--  BACK_END  -->
-  <section class="row level-back bg-01" id="level">
-    <h3>Mes Compétences Back</h3>
-    <div class="parcours-wrapper">
+  <section class="row flex justify-center bg-01">
+  <h3 class="text-red-800 pb-6">Mes Compétences Back</h3>
+    <div class="parcours-wrapper grid grid-cols-5">
       <?php
         $count = count($backEnd);
         for ($i = 0; $i < $count; $i++) {
       ?>
-        <div class="panel panel-front col-lg-3 col-md-4 col-sm-6 col-xs-12">
-          <div class="panel-heading">
+        <div class="panel">
+        <div class="panel-heading h-3/5">
             <h4><?php echo $backEnd[$i][0];?></h4>
           </div>
           <div class="panel-body">
@@ -163,62 +137,42 @@
               </div>
             </div>
           </div>
-        <?php
-            }
-        ?>
+        <?php } ?>
     </div>
   </section>
+
   <!--  PORTFOLIO  -->
  <!-- TODO - effectuer l'affichage en responsive Mobile  -->
-  <section class="row portfolio bg-02" id="portfolio">
-    <div class="title"><h3>Mes Réalisations</h3></div>
-    <div class="col">
+  <section class="row portfolio flex justify-center bg-02" id="portfolio">
+    <h3 class="text-red-800 pb-6">Mes Réalisations</h3>
       <div class="display">
         <div class="desktop">
-          <iframe id="desktop" title="responsiv" src="">
-          </iframe>
+          <iframe id="desktop" title="responsiv" src=""></iframe>
        </div>
       </div>
-      <div class="menuSite">
-        <button type="button" id="respBtn1" class="btn btn-secondary" onClick="displayProject('site1')">Portfolio</button>
-        <button type="button" id="respBtn2" class="btn btn-secondary" onClick="displayProject('site2')">Cristal{ID}</button>
-        <button type="button" id="respBtn3" class="btn btn-secondary" onClick="displayProject('site3')">Jacek jeznach</button>
-        <button type="button" id="respBtn4" class="btn btn-secondary" onClick="displayProject('site4')">Lorem Photos</button>
-        <button type="button" id="respBtn5" class="btn btn-secondary" onClick="displayProject('site5')"></button>
+      <div class="menuSite flex justify-center text-black">
+        <button type="button" id="respBtn2" class="text-black btn btn-outline-secondary" onClick="displayProject('site2')">Cristal{ID}</button>
+        <button type="button" id="respBtn3" class="text-black btn btn-outline-secondary" onClick="displayProject('site3')">Jacek jeznach</button>
+        <button type="button" id="respBtn4" class="text-black btn btn-outline-secondary" onClick="displayProject('site4')">Lorem Photos</button>
       </div>
-    </div>
   </section>
+
   <!-- CONTACT -->
-  <section class="row contact bg-01" id="contact">
-    <div class="title"><h3>Me Contacter</h3></div>
-    <div class="col col-4">
-      <p>Je suis intéressé par les opportunités de freelance.<br> 
-      Cependant, si vous avez d'autres demandes ou questions,<br>
-      n'hésitez pas à utiliser le formulaire.</p>
-    </div>
-    <div class="col col-6">
-      <div class="m-form">
-        <form id="register-form">
-          <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-          </div>
-          <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-          </div>
-          <div class="input-action">
-            <button type="submit">Envoyer</button>
-          </div>
-        </form>
-      </div>
-    </div>
+  <section class="row bg-01" id="contact">
+    <h3 class="text-red-800 pb-6">Me Contacter</h3>
+    <p class="text-xl font-semibold text-justify p-16">
+      Je suis intéressé par les opportunités de freelance.<br> 
+    Cependant, si vous avez d'autres demandes ou questions,<br>
+    veuillez  me contacter par <a href="mailto:nabet.eleazar@gmail.com">e-mail.</a></p>
   </section>
+
   <!-- FOOTER -->
-  <footer class="row bg-02">
-    <p class="copyright">Company Name © 2021</p>
+  <footer class="row flex text-center justify-around h-32 p-8 bg-02">
+    <p class="copyright text-xl">Company Name © 2021</p>
   </footer>
 </div>    
+
+
  <!--  JS Files  -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="js_files/main.js"></script>
