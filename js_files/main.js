efda97753b93285c3961e7119925e75ebe211643
NABET Eleazar <nabet.eleazar@gmail.com>
2022-04-28 21:31:00 +0200

    position navbar scroll

--- js_files/main.js
+++ js_files/main.js
@@ -5,15 +5,25 @@ jQuery(document).ready(function () {
   console.log('jQuery n\'est  pas inclus à la page.');
 }
 
+// BTN Scroll To Top
 $("h1").text();
-$(".scroll").click( function ()  {
+$(".scroll").click( function () {
    document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0;
 })
-});
+
+// Change style of navbar on scroll
+window.onscroll = function() {myFunction()};
+
+});  //Ready jQuery.
 
 
-/**  Message après chgrt de page   **/
-window.addEventListener('load', () => {
-  console.log("JS: Tout est bien chargé.")
-});
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) { 
+      $("nav").css("position","fixed").css("top","0").css("left","0").css("background","orange").css("width","100%").css("z-index","1");
+  }
+  else if (document.body.scrollTop < 2 || document.documentElement.scrollTop < 5) {
+      $("nav").css("position","relative").css("top","0").css("background","wheat");
+  }
+}
